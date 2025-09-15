3d350a48150dd0cc8d5079f49e1118c55c78d92b
Floris Barendsen <fpjbarendsen@gmail.com>
2025-09-15 15:07:33 +0200

    Stats route toegevoegd

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,10 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::get('/stats', function () {
+    return view('stats');
+})->middleware(['auth', 'verified'])->name('stats');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
