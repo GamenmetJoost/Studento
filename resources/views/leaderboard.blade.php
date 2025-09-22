c2214bea0fcb64851430a70fadb2453ef80b6ac8
Joost van Assenbergh <joosto55xi@gmail.com>
2025-09-22 12:43:13 +0200

    feat: add page title component

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -1,9 +1,7 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Leaderboard & Achievements') }}
-        </h2>
-    </x-slot>
+    <x-page-title>
+        Leaderboard & Achievements
+    </x-page-title>
 
     <div class="py-12" x-data="{ open: false, badge: null }">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
