8046d2dcb67c6eb97c93f0df2fe69eff44c37b4e
NeverEndingNiqht <fpjbarendsen@gmail.com>
2025-10-08 21:29:02 +0200

    Change: Update hover styles for navigation links

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -3,7 +3,7 @@
 @php
 $classes = ($active ?? false)
             ? 'inline-flex items-center px-1 pt-1 border-b-2 border-primary_blue text-sm font-medium leading-5 text-gray-900 dark:text-gray-100 focus:outline-none focus:border-primary_blue transition duration-150 ease-in-out'
-            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out';
+            : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-900 dark:hover:text-gray-100 hover:border-primary_blue focus:outline-none focus:text-gray-900 dark:focus:text-gray-100 focus:border-primary_blue transition duration-150 ease-in-out';
 @endphp
 
 <a {{ $attributes->merge(['class' => $classes]) }}>
