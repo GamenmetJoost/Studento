d24322269a6d44b615607652488f1bbc3ff1a9f1
NeverEndingNiqht <fpjbarendsen@gmail.com>
2025-10-10 12:51:16 +0200

    FIx: Wachtwoord vergeten pagina is nu ook NL

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('Wachtwoord vergeten? Geen probleem. Vul je e-mailadres in en we sturen je een link om je wachtwoord te resetten.') }}
     </div>
 
     <!-- Session Status -->
@@ -18,7 +18,7 @@
 
         <div class="flex items-center justify-end mt-4">
             <x-primary-button>
-                {{ __('Email Password Reset Link') }}
+                {{ __('Email wachtwoord reset link') }}
             </x-primary-button>
         </div>
     </form>
